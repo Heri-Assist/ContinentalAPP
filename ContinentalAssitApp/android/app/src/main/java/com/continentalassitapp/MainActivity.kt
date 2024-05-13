11afafec4b00534fdac06e812ebcb7be90b34f95
Heri-Assist <hvalencia@continentalassist.com>
2024-05-13 18:56:15 -0500

    actualizacion de react native 0.73.6 correcion de nuevas politicas de apple

--- ContinentalAssitApp/android/app/src/main/java/com/continentalassitapp/MainActivity.kt
+++ ContinentalAssitApp/android/app/src/main/java/com/continentalassitapp/MainActivity.kt
@@ -7,7 +7,6 @@ import com.facebook.react.ReactActivity
 import org.devio.rn.splashscreen.SplashScreen // here
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-// import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
